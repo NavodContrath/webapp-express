47aed4a12da813e5f54b3f8c5f49be47f89a093c
NavodContrath <navod.contrath@gmail.com>
2025-04-15 16:09:53 +0200

    feat: enhance show function to include movie reviews in response

--- backend_movies/controllers/movies_controller.js
+++ backend_movies/controllers/movies_controller.js
@@ -10,13 +10,25 @@ function index(req, res) {
 }
 
 function show(req, res) {
+    //take id from request
     const id = Number(req.params.id)
+    //define sql query to get movie with specific id
     const sql = 'SELECT * FROM movies WHERE id=?'
+    //define sql query to get reviews with specific id
+    const reviewSql = 'SELECT reviews.name,reviews.text,reviews.vote FROM reviews WHERE id=?'
+    //execute movie's query
     connection.query(sql, [id], (err, results) => {
         if (err) return res.status(500).json({ error: err.message })
         if (results.length === 0) return res.status(404).json({ error: 'movie not found' })
         const movie = results[0]
-        res.json(movie)
+        //execute review's query
+        connection.query(reviewSql, [id], (err, review) => {
+            if (err) return res.status(500).json({ erro: err.message })
+            //add review to movie object with DOT notation
+            movie.reviews = review
+            res.json(movie)
+        })
+
     })
 }
 
