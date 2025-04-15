04f97e5aad72c159f7afe1dbf81c3d9db5fc00ec
NavodContrath <navod.contrath@gmail.com>
2025-04-15 16:03:51 +0200

    feat: implement show function in movies controller and update router for movie details retrieval

--- backend_movies/controllers/movies_controller.js
+++ backend_movies/controllers/movies_controller.js
@@ -7,9 +7,20 @@ function index(req, res) {
         if (err) return res.status(500).json({ error: err.message })
         res.json(results)
     })
+}
 
+function show(req, res) {
+    const id = Number(req.params.id)
+    const sql = 'SELECT * FROM movies WHERE id=?'
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ error: err.message })
+        if (results.length === 0) return res.status(404).json({ error: 'movie not found' })
+        const movie = results[0]
+        res.json(movie)
+    })
 }
 
 module.exports = {
     index,
+    show
 }
--- backend_movies/routers/movies.js
+++ backend_movies/routers/movies.js
@@ -1,7 +1,9 @@
 const moviesController = require('../controllers/movies_controller')
 const router = require('express').Router()
 
-
+//index
 router.get('/', moviesController.index)
+//show
+router.get('/:id', moviesController.show)
 
 module.exports = router
